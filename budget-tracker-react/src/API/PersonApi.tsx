554de8dda64fa8721c446424e01d54b141b35634
ak13 <i549082@fontysict.nl>
2025-11-04 01:48:54 +0100

    get income

--- budget-tracker-react/src/API/PersonApi.tsx
+++ budget-tracker-react/src/API/PersonApi.tsx
@@ -4,6 +4,14 @@ export interface BalanceResponse {
     balance: number;
 }
 
+export interface ExpensesResponse {
+    totalExpenses: number;
+}
+
+export interface IncomeResponse {
+    totalIncome: number;
+}
+
 const api = ky.create({
     prefixUrl: 'http://localhost:8080/api/person',
     timeout: 5000,
@@ -21,4 +29,24 @@ export async function getUserBalance(userId: number): Promise<number> {
     } catch (error) {
         throw new Error(`Failed to fetch user balance: ${error}`);
     }
+}
+
+export async function getTotalExpense(userId: number): Promise<number> {
+       try {
+        const response = await api.get(`${userId}/expense`);
+        const data: number = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(`Failed to fetch user balance: ${error}`);
+    }
+}
+
+export async function getTotalIncome(userId: number): Promise<number> {
+    try {
+        const response = await api.get(`${userId}/income`);
+        const data: number = await response.json();
+        return data;
+    } catch (error) {
+        throw new Error(`Failed to fetch user income: ${error}`);
+    }   
 }
--- budget-tracker-react/src/assets/Layout/Dashboard.tsx
+++ budget-tracker-react/src/assets/Layout/Dashboard.tsx
@@ -1,13 +1,19 @@
 import styles from './Dashboard.module.css';
 import { useEffect, useState } from "react";
 import { getUserBalance } from '../../API/PersonApi.tsx';
+import { getTotalExpense } from '../../API/PersonApi.tsx';
+import { getTotalIncome } from '../../API/PersonApi.tsx';
 
 function Dashboard(){
     const [balance, setBalance] = useState<number | null>(null);
+    const [totalExpense, setTotalExpense] = useState<number | null>(null);
+    const [totalIncome, setTotalIncome] = useState<number | null>(null);
 
     useEffect(() => {
         const userId = 1; // dummy user Id
         getUserBalance(userId).then(setBalance).catch(console.error);
+        getTotalExpense(userId).then(setTotalExpense).catch(console.error);
+        getTotalIncome(userId).then(setTotalIncome).catch(console.error);
     }, []);
 
 
@@ -25,11 +31,11 @@ function Dashboard(){
                 
                 <div className={styles.card}>
                     <h3>Total Income</h3>
-                    <p className={styles.balance}>$0</p>
+                    <p className={styles.balance}>+${totalIncome}</p>
                 </div>
                 <div className={styles.card}>
                     <h3>Total Expenses</h3>
-                    <p className={styles.balance}>$0</p>
+                    <p className={styles.balance}>-${totalExpense}</p>
                 </div>
 
                 <div className={styles.balanceCards}>
